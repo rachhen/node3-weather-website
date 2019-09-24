1c835d4b9278f4388784f1843d2874d00e21aaaa
Rachhen <rachhen.it@outlook.com>
2019-09-24 23:24:55 +0700

    Add high and low temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,7 +18,11 @@ const forecast = (latitude, longitude, callback) => {
 				body.daily.data[0].summary +
 					" It is currently " +
 					body.currently.temperature +
-					" degress out. There is a " +
+					" degress out. This high today is " +
+					body.daily.data[0].temperatureHigh +
+					" with a low of " +
+					body.daily.data[0].temperatureLow +
+					". There is a " +
 					body.currently.precipProbability +
 					"% chance of rain. " +
 					body.daily.summary
